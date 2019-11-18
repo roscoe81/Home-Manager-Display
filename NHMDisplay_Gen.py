af9c400649a41947f8b0c7c162ae5e002537c2df
Ross Fowler <rifowler@internode.on.net>
2019-11-18 13:01:39 +1100

    Update NHMDisplay_Gen.py

--- NHMDisplay_Gen.py
+++ NHMDisplay_Gen.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-# Northcliff Home Manager Display - Version 3.10 Gen
-# Requires Home Manager >= Version 8.5
+# Northcliff Home Manager Display - Version 3.13-Gen
+# Requires Home Manager >= Version 8.14
 import time
 import paho.mqtt.client as mqtt
 import json
@@ -10,7 +10,8 @@ from sense_hat import SenseHat
 
 class NorthcliffDisplay(object): # The class for the main display code
     def __init__(self):
-        self.homebridge_outgoing_mqtt_topic = 'homebridge/to/set'
+        self.homebridge_outgoing_mqtt_topic='homebridge/to/set'
+        self.domoticz_incoming_mqtt_topic='domoticz/in'
         self.multisensor_names = ['Living', 'Study', 'Kitchen', 'North', 'South', 'Main', 'Rear Balcony', 'North Balcony', 'South Balcony']
         self.motion_map={'Living Motion':(6,4), 'Study Motion':(2,3), 'Kitchen Motion':(3,0), 'North Motion':(1,6), 'South Motion':(1,0), 'Main Motion':(5,6),
                           'Rear Balcony Motion':(0,6), 'North Balcony Motion':(7,6), 'South Balcony Motion':(7,0)}
@@ -27,15 +28,24 @@ class NorthcliffDisplay(object): # The class for the main display code
         self.barometer_calibration_offset=-3
         self.barometer_change_map=(0,4)
         self.barometer_history = [0.00 for x in range (10)]
-        #self.barometer_history = [1023.6, 1023.3, 1023.2, 1023.3, 1023.2, 1023.4, 1023.5, 1023.7, 1023.1, 1024.0]
         self.weather_forecast=[[0,0],[0,0],[0,0]]
         self.wind_forecast_map=(0,0)
         self.rain_forecast_map=(0,1)
         self.temp_forecast_map=(0,2)
-        self.forecast_barometer_map={'No Change':[[120,0],[120,0],[120,0]], 'Clearing and Colder':[[120,100],[120,100],[180,100]], 'Rain and Wind':[[60,100],[240,100],[120,0]], 'Storm':[[30,100],[240,100],[180,100]],
-                                      'Storm and Gale':[[0,100],[240,100],[180,100]], 'Strong Wind Warning':[[30,100],[120,0],[120,0]], 'Gale Warning': [[0,100],[120,0],[120,0]], 'Poorer Weather': [[60,100],[180,100],[180,100]],
-                                      'Fair Weather with Slight Temp Change': [[120,100],[120,100],[150,100]], 'No Change and Rain in 24 Hours': [[120,0],[180,100],[120,0]], 'Rain, Wind and Higher Temp': [[80,100],[180,100],[60,100]],
-                                      'Fair Weather':[[120,100],[120,100],[120,100]], 'Fair Weather Weather with No Marked Temp Change': [[120,100],[120,100],[120,0]], 'Fair Weather and Slowly Rising Temp': [[120,100],[120,100],[60,100]], 'Warming Trend': [[120,0],[120,0],[30,100]]}
+        self.forecast_barometer_map={'No Change':[[[120,0],[120,0],[120,0]],'0'], 'Clearing and Colder':[[[120,100],[120,100],[180,100]],'0'], 'Rain and Wind':[[[60,100],[240,100],[120,0]],'3'], 'Storm':[[[30,100],[240,100],[180,100]],'4'],
+                                      'Storm and Gale':[[[0,100],[240,100],[180,100]],'4'], 'Strong Wind Warning':[[[30,100],[120,0],[120,0]],'3'], 'Gale Warning': [[[0,100],[120,0],[120,0]],'4'],
+                                      'Poorer Weather': [[[60,100],[180,100],[180,100]],'3'],
+                                      'Fair Weather with Slight Temp Change': [[[120,100],[120,100],[150,100]],'1'], 'No Change and Rain in 24 Hours': [[[120,0],[180,100],[120,0]],'3'],
+                                      'Rain, Wind and Higher Temp': [[[80,100],[180,100],[60,100]],'3'],
+                                      'Fair Weather':[[[120,100],[120,100],[120,100]],'1'], 'Fair Weather Weather with No Marked Temp Change': [[[120,100],[120,100],[120,0]],'0'],
+                                      'Fair Weather and Slowly Rising Temp': [[[120,100],[120,100],[60,100]],'1'], 'Warming Trend': [[[120,0],[120,0],[30,100]],'1']}
+        self.domoticz_barometer_format={'idx':767,'nvalue':0}
+        self.domoticz_barometer_level='0'
+        self.domoticz_barometer_forecast='0'
+        self.aquarium_ph_map=(5,0)
+        self.aquarium_nh3_map=(5,1)
+        self.aquarium_temp_map=(5,2)
+        self.aquarium_idx_map={'ph':769,'nh3':770,'temp':772}
         self.low_light=False
      
     def on_connect(self, client, userdata, flags, rc):
@@ -68,13 +78,23 @@ class NorthcliffDisplay(object): # The class for the main display code
             else:
                 #print("Ignored json", parsed_json)
                 pass
+        elif msg.topic == self.domoticz_incoming_mqtt_topic: # If it's a message sent to Domoticz
+            #print('Received Domoticz In Message', parsed_json)
+            aquarium_idx_found=False
+            for sensor in self.aquarium_idx_map:
+                if parsed_json['idx']==self.aquarium_idx_map[sensor]:
+                    aquarium_idx_found=True
+            if aquarium_idx_found:
+                self.process_aquarium(parsed_json)
+        else:
+            pass # Ignore other messages    
 
     def process_air_purifier_filter(self, parsed_json):
         if parsed_json['value']==1: # Filter needs changing
             self.load_display_buffer(self.air_purifier_filter_map[parsed_json['name']][0], self.air_purifier_filter_map[parsed_json['name']][1], [0,100,100]) # Red
         else: # Filter is OK
             self.load_display_buffer(self.air_purifier_filter_map[parsed_json['name']][0], self.air_purifier_filter_map[parsed_json['name']][1], [0,0,0]) # Off                                                   
-                
+
     def process_aircon(self, parsed_json):
         #print("Process Aircon", parsed_json)
         if parsed_json['service_name'] == 'Remote Operation' and parsed_json['value'] == False:
@@ -151,18 +171,62 @@ class NorthcliffDisplay(object): # The class for the main display code
     def process_barometer(self):
         barometer_log_time = time.time()
         barometer=round(sense.get_pressure(),2)+self.barometer_calibration_offset
-        valid_barometer_history, barometer_change = self.log_barometer(barometer)
-        #self.print_update('Barometer Reading of '+str(barometer)+' millibars on ')
-        if barometer>1023:
-            hue=0
-        elif barometer<1003:
-            hue=240
+        if barometer>500: # Only record valid barometer readings. Caters for startup mode
+            valid_barometer_reading=True
+            valid_barometer_history, barometer_change = self.log_barometer(barometer)
+            self.print_update('Barometer Reading of '+str(barometer)+' millibars on ')
+            if barometer>1023:
+                hue=0
+            elif barometer<1003:
+                hue=240
+            else:
+                hue=int((1023-barometer)*12)
+            self.load_display_buffer(self.barometer_map[0], self.barometer_map[1], [hue,100,100])
+            self.domoticz_barometer=str(round(barometer,1))
+            if valid_barometer_history == True:
+                self.process_barometer_change(barometer_change, barometer)
+            else:
+                domoticz_json = self.domoticz_barometer_format
+                domoticz_json['svalue'] = self.domoticz_barometer+';0'
+                client.publish(self.domoticz_incoming_mqtt_topic, json.dumps(domoticz_json))
         else:
-            hue=int((1023-barometer)*12)
-        self.load_display_buffer(self.barometer_map[0], self.barometer_map[1], [hue,100,100])
-        if valid_barometer_history == True:
-            self.process_barometer_change(barometer_change, barometer)
-        return barometer_log_time
+            valid_barometer_reading=False
+        return valid_barometer_reading, barometer_log_time
+
+    def process_aquarium(self,parsed_json):
+        print('Aquarium Message', parsed_json)
+        if parsed_json['idx']==self.aquarium_idx_map['ph']:
+            print('ph:', parsed_json['svalue'], self.aquarium_ph_map[0], self.aquarium_ph_map[1])
+            ph=float(parsed_json['svalue'])
+            if ph<6.5:
+                hue=0
+            elif ph>7.5:
+                hue=240
+            else:
+                hue=int((ph-6.5)*240)
+            self.load_display_buffer(self.aquarium_ph_map[0], self.aquarium_ph_map[1], [hue,100,100])
+        elif parsed_json['idx']==self.aquarium_idx_map['nh3']:
+            print('nh3:', parsed_json['svalue'], self.aquarium_nh3_map[0], self.aquarium_nh3_map[1])
+            nh3=float(parsed_json['svalue'])
+            if nh3<0:
+                hue=120
+            elif nh3>0.2:
+                hue=0
+            else:
+                hue=int(120-nh3*600)
+            self.load_display_buffer(self.aquarium_nh3_map[0], self.aquarium_nh3_map[1], [hue,100,100])
+        elif parsed_json['idx']==self.aquarium_idx_map['temp']:
+            print('temp:', parsed_json['svalue'], self.aquarium_temp_map[0], self.aquarium_temp_map[1])
+            temp=float(parsed_json['svalue'])
+            if temp<22:
+                hue=240
+            elif temp>28:
+                hue=0
+            else:
+                hue=int((temp-28)*-40)
+            self.load_display_buffer(self.aquarium_temp_map[0], self.aquarium_temp_map[1], [hue,100,100])
+        else:
+            pass
             
     def log_barometer(self, barometer): # Logs 3 hours of barometer readings, taken every 18 minutes
         three_hour_barometer=self.barometer_history[9] # Capture barometer reading from 3 hours ago
@@ -187,10 +251,14 @@ class NorthcliffDisplay(object): # The class for the main display code
         else:
             hue=int(120-barometer_change*30)
         self.load_display_buffer(self.barometer_change_map[0], self.barometer_change_map[1], [hue,100,100])
-        led_forecast, forecast = self.analyse_barometer(barometer_change, barometer, self.forecast_barometer_map)
+        led_forecast, forecast, domoticz_forecast = self.analyse_barometer(barometer_change, barometer, self.forecast_barometer_map)
         self.load_display_buffer(self.wind_forecast_map[0], self.wind_forecast_map[1], [led_forecast[0][0],led_forecast[0][1],100])
         self.load_display_buffer(self.rain_forecast_map[0], self.rain_forecast_map[1], [led_forecast[1][0],led_forecast[1][1],100])
         self.load_display_buffer(self.temp_forecast_map[0], self.temp_forecast_map[1], [led_forecast[2][0],led_forecast[2][1],100])
+        self.domoticz_barometer_forecast=domoticz_forecast
+        domoticz_json = self.domoticz_barometer_format
+        domoticz_json['svalue'] = self.domoticz_barometer +';'+self.domoticz_barometer_forecast
+        client.publish('domoticz/in', json.dumps(domoticz_json))
         self.print_update('3 hour barometer change is '+str(round(barometer_change,1))+' millibars with a current reading of '+str(round(barometer,1))+' millibars. The weather forecast is "'+forecast+'" on ') 
 
     def analyse_barometer(self, barometer_change, barometer, forecast_barometer_map):
@@ -251,8 +319,9 @@ class NorthcliffDisplay(object): # The class for the main display code
                 forecast = 'Fair Weather and Slowly Rising Temp'
             else:
                 forecast = 'Warming Trend'
-        led_forecast=forecast_barometer_map[forecast]
-        return led_forecast, forecast
+        led_forecast=forecast_barometer_map[forecast][0]
+        domoticz_forecast=forecast_barometer_map[forecast][1]
+        return led_forecast, forecast, domoticz_forecast
 
     def load_display_buffer(self,x,y,h_s_v):
         red,green,blue=self.set_led_colour(h_s_v[0], h_s_v[1], h_s_v[2])
@@ -311,12 +380,14 @@ class NorthcliffDisplay(object): # The class for the main display code
     def run(self):
         self.print_update("Northcliff Home Manager Display started on ")
         try:
-            barometer_log_time = self.process_barometer()
+            valid_barometer_reading=False
+            while valid_barometer_reading==False: # Wait for valid barometer reading
+                valid_barometer_reading, barometer_log_time = self.process_barometer()
             while True: # Run display in continuous loop
                 time.sleep(0.5)
                 self.drive_display()
                 if (time.time() - barometer_log_time) >= 1080: # Update the barometer log if last update was >= 18 minutes ago
-                    barometer_log_time = self.process_barometer()     
+                    valid_barometer_reading, barometer_log_time = self.process_barometer()     
         except KeyboardInterrupt: # Shutdown on ctrl C
             # Shutdown main program
             print('Barometer Log:', self.barometer_history)
@@ -336,6 +407,7 @@ if __name__ == '__main__': # This is where to overall code kicks off
     # Blocking call that processes network traffic, dispatches callbacks and handles reconnecting.
     client.loop_start()
     client.subscribe(dsp.homebridge_outgoing_mqtt_topic) # Subscribe to mqtt messages from Home Manager to Homebridge
+    client.subscribe(dsp.domoticz_incoming_mqtt_topic) # Subscribe to mqtt messages from Home Manager to Domoticz
     dsp.run()
 
 
